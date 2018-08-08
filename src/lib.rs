7ac8540e1a95759c6d694c4fa2d6456195558972
Taisuke Hori <taisuke.horix@gmail.com>
2018-08-08 21:35:22 +0900

    Refactoring. Documents.

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,55 @@
-use nom::*;
+//! # Parser for Carthage files.
+//!
+//! carthage_rs provides the avility to parse Carthage's
+//! `Cartfile`, `Cartfile.private` and `Carthage.resolved`.
+//!
+//!
+//! # Examples
+//! ```
+//! use cartfile_rs::parse_cartfile;
+//! let dependencies = parse_cartfile(
+//!         r#"
+//! github "antitypical/Result" ~> 3.2
+//! github "Carthage/ReactiveTask" ~> 0.14
+//! "#,
+//! );
+//!
+//! assert_eq!(2, dependencies.len());
+//! ```
+
 use nom::types::*;
+use nom::*;
 
 type Input<'a> = CompleteByteSlice<'a>;
 
+#[allow(dead_code)]
+fn slice_to_str(x: Input) -> Result<&str, std::str::Utf8Error> {
+    std::str::from_utf8(&x)
+}
+
+#[allow(dead_code)]
+fn is_newline(c: u8) -> bool {
+    c != b'\n' && c != b'\r'
+}
+
+/// Carthage dependency.
 #[derive(Debug, PartialEq)]
-pub enum OriginType {
+pub struct Dependency<'a> {
+    pub kind: Kind,
+    pub location: &'a str,
+    pub requirement: Option<Requirement<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Kind {
     Binary,
     Git,
     GitHub,
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Library<'a> {
-  pub origin_type: OriginType,
-  pub origin: &'a str,
-  pub version_requirement: Option<VersionRequirement<'a>>,
-}
-
-#[derive(Debug, PartialEq)]
-pub struct VersionConstraint<'a> {
-    pub version_op: VersionOp,
-    pub version: &'a str,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum VersionRequirement<'a> {
-    Version(VersionConstraint<'a>),
+pub enum Requirement<'a> {
+    Version { op: VersionOp, version: &'a str },
     Pin(&'a str),
 }
 
@@ -36,19 +60,47 @@ pub enum VersionOp {
     CompatibleWith,
 }
 
-named!(origin_type<Input, OriginType>,
-       alt_complete!(
-           map!(tag!("binary"), |_| OriginType::Binary) |
-           map!(tag!("github"), |_| OriginType::GitHub) |
-           map!(tag!("git"), |_| OriginType::Git)
+named!(dependency<Input, Dependency>,
+       do_parse!(
+           kind: kind >>
+           multispace0 >>
+           location: location >>
+           requirement: opt!(preceded!(multispace0, alt!(
+               version_constraint |
+               map!(version_pin, |c| Requirement::Pin(c))
+           ))) >>
+           ( Dependency { kind, location, requirement } )
        )
 );
 
-named!(origin<Input, &str>,
-       map_res!(delimited!(tag!("\""), take_until!("\""), tag!("\"")), s2u)
+named!(kind<Input, Kind>,
+       alt_complete!(
+           map!(tag!("binary"), |_| Kind::Binary) |
+           map!(tag!("github"), |_| Kind::GitHub) |
+           map!(tag!("git"), |_| Kind::Git)
+       )
 );
 
-named!(version_op<Input, VersionOp>,
+#[test]
+fn parse_kind() {
+    assert_eq!(
+        kind(b"binary"[..].into()),
+        Ok((b""[..].into(), Kind::Binary))
+    );
+
+    assert_eq!(kind(b"git"[..].into()), Ok((b""[..].into(), Kind::Git)));
+
+    assert_eq!(
+        kind(b"github"[..].into()),
+        Ok((b""[..].into(), Kind::GitHub))
+    );
+}
+
+named!(location<Input, &str>,
+       map_res!(delimited!(tag!("\""), take_until!("\""), tag!("\"")), slice_to_str)
+);
+
+named!(op<Input, VersionOp>,
        alt_complete!(
            map!(tag!("=="), |_| VersionOp::Equal) |
            map!(tag!("~>"), |_| VersionOp::CompatibleWith) |
@@ -56,61 +108,115 @@ named!(version_op<Input, VersionOp>,
        )
 );
 
+#[test]
+fn parse_op() {
+    assert_eq!(
+        op(br#"=="#[..].into()),
+        Ok((b""[..].into(), VersionOp::Equal))
+    );
+    assert_eq!(
+        op(br#"~>"#[..].into()),
+        Ok((b""[..].into(), VersionOp::CompatibleWith))
+    );
+    assert_eq!(
+        op(br#">="#[..].into()),
+        Ok((b""[..].into(), VersionOp::AtLeast))
+    );
+    assert_eq!(
+        op(br#"a"#[..].into()),
+        Err(Err::Error(Context::Code(
+            br#"a"#[..].into(),
+            ErrorKind::Alt
+        )))
+    );
+}
+
 named!(version<Input, &str>,
-       map_res!(take_while!(|c| is_digit(c) || c == b'.'), s2u)
+       map_res!(take_while1!(|c| is_digit(c) || c == b'.'), slice_to_str)
 );
 
-named!(version_constraint<Input, VersionConstraint>,
+#[test]
+fn parse_version() {
+    assert_eq!(version(br#"0"#[..].into()), Ok((b""[..].into(), "0")));
+    assert_eq!(
+        version(br#"0.0.0"#[..].into()),
+        Ok((b""[..].into(), "0.0.0"))
+    );
+    assert_eq!(
+        version(br#"a"#[..].into()),
+        Err(Err::Error(Context::Code(
+            br#"a"#[..].into(),
+            ErrorKind::TakeWhile1
+        )))
+    );
+}
+
+named!(version_constraint<Input, Requirement>,
        do_parse!(
-           version_op: version_op >>
+           op: op >>
            multispace0 >>
            version: version >>
-           ( VersionConstraint { version_op, version } )
+           ( Requirement::Version { op, version } )
        )
 );
+
+#[test]
+fn parse_version_constraint() {
+    assert_eq!(
+        version_constraint(br#"== 1.2"#[..].into()),
+        Ok((
+            b""[..].into(),
+            Requirement::Version {
+                op: VersionOp::Equal,
+                version: "1.2"
+            }
+        ))
+    );
+
+    assert_eq!(
+        version_constraint(br#"~>0"#[..].into()),
+        Ok((
+            b""[..].into(),
+            Requirement::Version {
+                op: VersionOp::CompatibleWith,
+                version: "0"
+            }
+        ))
+    );
+}
 
 named!(version_pin<Input, &str>,
   map_res!(delimited!(tag!("\""), take_until!("\""), tag!("\"")),
-           s2u)
+           slice_to_str)
 );
 
-#[allow(dead_code)]
-fn s2u(x: Input) -> Result<&str, std::str::Utf8Error> {
-    std::str::from_utf8(&x)
+#[test]
+fn parse_version_pin() {
+    assert_eq!(
+        version_pin(br#""foo bar""#[..].into()),
+        Ok((b""[..].into(), "foo bar"))
+    )
 }
 
-named!(library<Input, Library>,
-       do_parse!(
-           origin_type: origin_type >>
-           multispace0 >>
-           origin: origin >>
-           version_requirement: opt!(preceded!(multispace0, alt!(
-               map!(version_constraint, |c| VersionRequirement::Version(c)) |
-               map!(version_pin, |c| VersionRequirement::Pin(c))
-           ))) >>
-           ( Library { origin_type, origin, version_requirement } ) 
-       )
-);
-
-named!(line<Input, Option<Library>>,
+named!(line<Input, Option<Dependency>>,
        do_parse!(
            multispace0 >>
-           library: opt!(library) >>
+           dependency: opt!(dependency) >>
            opt!(comment) >>
-           take_while!( |c| c != b'\n' && c!= b'\r' ) >>
+           take_while!( is_newline ) >>
            opt!(eol) >>
-           ( library )
+           ( dependency )
        )
 );
 
 named!(comment<Input, Input>,
        recognize!(
            preceded!(tag!("#"),
-                     take_while!( |c| c != b'\n' && c!= b'\r' ))
+                     take_while!( is_newline ))
        )
 );
 
-named!(cartfile<Input, Vec<Library>>,
+named!(cartfile<Input, Vec<Dependency>>,
        do_parse!(
            lines: many0!( line ) >>
            ( lines.into_iter().flat_map(|e| e).collect() )
@@ -118,66 +224,81 @@ named!(cartfile<Input, Vec<Library>>,
 );
 
 #[test]
-fn parse_library() {
-    assert_eq!(library(br#"github "hoge" "foo""#[..].into()),
-               Ok((b""[..].into(),
-                   Library{ origin_type: OriginType::GitHub,
-                            origin: "hoge",
-                            version_requirement: Some(VersionRequirement::Pin("foo"))
-                   })));
+fn parse_dependency() {
+    assert_eq!(
+        dependency(br#"github "hoge" "foo""#[..].into()),
+        Ok((
+            b""[..].into(),
+            Dependency {
+                kind: Kind::GitHub,
+                location: "hoge",
+                requirement: Some(Requirement::Pin("foo"))
+            }
+        ))
+    );
 
-    assert_eq!(library(br#"github "hoge" == 2.3"#[..].into()),
-               Ok((b""[..].into(),
-                   Library{ origin_type: OriginType::GitHub,
-                            origin: "hoge",
-                            version_requirement: Some(VersionRequirement::Version(VersionConstraint{ version_op: VersionOp::Equal, version: "2.3" }))
-                   })));
+    assert_eq!(
+        dependency(br#"github "hoge" == 2.3"#[..].into()),
+        Ok((
+            b""[..].into(),
+            Dependency {
+                kind: Kind::GitHub,
+                location: "hoge",
+                requirement: Some(Requirement::Version {
+                    op: VersionOp::Equal,
+                    version: "2.3"
+                })
+            }
+        ))
+    );
 
-    assert_eq!(library(br#"github "hoge""#[..].into()),
-               Ok((b""[..].into(),
-                   Library{ origin_type: OriginType::GitHub,
-                            origin: "hoge",
-                            version_requirement: None,
-                   })));
+    assert_eq!(
+        dependency(br#"github "hoge""#[..].into()),
+        Ok((
+            b""[..].into(),
+            Dependency {
+                kind: Kind::GitHub,
+                location: "hoge",
+                requirement: None,
+            }
+        ))
+    );
 }
-
-#[test]
-fn parse_version() {
-    assert_eq!(version(b"0.1 "[..].into()), Ok((b" "[..].into(), "0.1")));
-}
-
-#[test]
-fn parse_origin_type() {
-  assert_eq!(origin_type(b"binary"[..].into()), Ok((b""[..].into(), OriginType::Binary)));
-  assert_eq!(origin_type(b"git"[..].into()), Ok((b""[..].into(), OriginType::Git)));
-  assert_eq!(origin_type(b"github"[..].into()), Ok((b""[..].into(), OriginType::GitHub)));
-}
-
 
 #[test]
 fn parse_comment() {
-  assert_eq!(comment(b"# binary\n# foo"[..].into()), Ok((b"\n# foo"[..].into(), b"# binary"[..].into())));
-  assert_eq!(comment(b"# binary # foo"[..].into()), Ok((b""[..].into(), b"# binary # foo"[..].into())));
+    assert_eq!(
+        comment(b"# binary\n# foo"[..].into()),
+        Ok((b"\n# foo"[..].into(), b"# binary"[..].into()))
+    );
+
+    assert_eq!(
+        comment(b"# binary # foo"[..].into()),
+        Ok((b""[..].into(), b"# binary # foo"[..].into()))
+    );
 }
 
-
-#[test]
-fn parse_cartfile() {
-  let r = cartfile(br#"
-# hoge
-
-   github     "hoge"                       
-
-  binary                  "foo"   ~>           2.10
-binary"foo">=2.10
-binary "foo" "bar"
-
-"#[..].into());
-    println!("====");
+/// Parse `Cartfile`.
+///
+/// # Examples
+///
+/// ```
+/// use cartfile_rs::parse_cartfile;
+/// let dependencies = parse_cartfile(
+///         r#"
+/// github "antitypical/Result" ~> 3.2
+/// github "Carthage/ReactiveTask" ~> 0.14
+/// "#,
+/// );
+///
+/// assert_eq!(2, dependencies.len());
+/// ```
+pub fn parse_cartfile(s: &str) -> Vec<Dependency> {
+    let r = cartfile(s.as_bytes().into());
 
     if let Ok((_, libs)) = r {
-        for lib in libs.iter() {
-            println!("{:?}", lib);
-        }
+        return libs;
+    } else {
+        return vec![];
     }
 }
---
+++ tests/cartfile_test.rs
@@ -0,0 +1,48 @@
+use cartfile_rs::*;
+
+#[test]
+fn test_parse_cartfile() {
+    let dependencies = parse_cartfile(
+        r#"
+# hoge
+
+   github     "hoge"                       
+
+  binary                  "foo"   ~>           2.10
+binary"foo">=2.10
+binary "foo" "bar"
+
+"#,
+    );
+
+    assert_eq!(dependencies.len(), 4);
+}
+
+#[test]
+fn test_cartfile_0() {
+    let dependencies = parse_cartfile(
+        r#"
+github "antitypical/Result" ~> 3.2
+github "Carthage/ReactiveTask" ~> 0.14
+github "Carthage/Commandant" ~> 0.14
+github "jdhealy/PrettyColors" ~> 5.0
+github "ReactiveCocoa/ReactiveSwift" ~> 3.1
+github "mdiep/Tentacle" ~> 0.11
+github "thoughtbot/Curry" ~> 4.0
+"#,
+    );
+
+    assert_eq!(
+        dependencies[0],
+        Dependency {
+            kind: Kind::GitHub,
+            location: "antitypical/Result",
+            requirement: Some(Requirement::Version {
+                op: VersionOp::CompatibleWith,
+                version: "3.2"
+            }),
+        }
+    );
+
+    assert_eq!(dependencies.len(), 7);
+}
